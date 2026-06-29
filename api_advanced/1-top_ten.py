89eec8c9e616ef98e7616c93de2e1b58e1df7943
nabdurahim-art <n.abdurahim@alustudent.com>
2026-06-29 01:15:32 -0700

    modified 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,8 +1,9 @@
+
 #!/usr/bin/python3
 """Module that queries Reddit API and prints top 10 hot posts"""
 import requests
-
-
+ 
+ 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
